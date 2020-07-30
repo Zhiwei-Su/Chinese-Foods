8079eab315ede3de5b369313e915277bf861d8ae
Titus Sudarno <43189799+titus-panjaya-sud@users.noreply.github.com>
2020-07-30 02:53:24 -0700

    Update Map.html

---
+++ js/mapdata.js
@@ -0,0 +1,279 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    CHN1150: {
+      name: "Gansu",
+      color: "#F2EAD2",
+      description: "Gansu is the doorway to the Silk Road of China, where foreign trades have been ongoing for past 2500 years.",
+      hover_color: "#D65F14",
+      //url: "http://facebook.com"
+    },
+    CHN1151: {
+      name: "Qinghai",
+      color: "#A4DCDC",
+      description: "Locating at the central, this place have been a melting pot for Chinese, Mongolian, Tibetan and Turkic culture for the past centuries.",
+      hover_color: "#D65F14"
+    },
+    CHN1152: {
+      name: "Guangxi",
+      color: "#8AA0A6",
+      description: "Bordering Vietnam, this place is the gateway to Southeast Asia.",
+      hover_color: "#D65F14"
+    },
+    CHN1153: {
+      name: "Guizhou",
+      color: "#EAB1CA",
+      description: "The world-famous Chili - Lao Gan Ma (老乾媽） comes from here.",
+      hover_color: "#D65F14"
+    },
+    CHN1154: {
+      name: "Chongqing",
+      color: "#226A7B",
+      description: "If you don't like spicy hotpot, just avoid here.",
+      hover_color: "#D65F14"
+    },
+    CHN1155: {
+      name: "Beijing",
+      color: "#E04A3E",
+      description: "Capital of China; Famous for the imperial food served for centuries.",
+      hover_color: "#D65F14"
+    },
+    CHN1178: {
+      name: "Fujian",
+      color: "#F4EBD6",
+      description: "Lychee in Fujian is the best unarguably.",
+      hover_color: "#D65F14"
+    },
+    CHN1179: {
+      name: "Anhui",
+      color: "#B8C09D",
+      description: "This is the foodbasket of China. The abundance of food have resulted in the wide variety of cuisine here.",
+      hover_color: "#D65F14"
+    },
+    CHN1180: {
+      name: "Guangdong",
+      color: "#D8D079",
+      description: "The homeland of Dimsum.",
+      hover_color: "#D65F14"
+    },
+    CHN1662: {
+      name: "Xizang",
+      color: "#F2F0DF",
+      description: "The heartland of Tibetian Buddhism, the people enjoy a clean diet from the sacrad mountains in Tibet.",
+      hover_color: "#D65F14"
+    },
+    CHN1756: {
+      name: "Xinjiang",
+      color: "#72706D",
+      description: "Homeland of the Muslim-Chinese Community; their roasted lamb is undefeatable!",
+      hover_color: "#D65F14"
+    },
+    CHN1775: {
+      name: "Hainan",
+      color: "#225353",
+      description: "Hainan is like the Florida of China, but thankfully we do not have \"Florida Man\"",
+      hover_color: "#D65F14"
+    },
+    CHN1803: {
+      name: "Ningxia",
+      color: "#CC8C56",
+      description: "If you like Halal food, Ningxia is the place to go!",
+      hover_color: "#D65F14"
+    },
+    CHN1804: {
+      name: "Shaanxi",
+      color: "#1B2B35",
+      description: "This used to be the Capital of many Ancient Chinese Dynasties for 1000 years.",
+      hover_color: "#D65F14"
+    },
+    CHN1805: {
+      name: "Shanxi",
+      color: "#31CC6F",
+      description: "They have a dish called \"Cat's Ear\", thankfully it's not from cats.",
+      hover_color: "#D65F14"
+    },
+    CHN1807: {
+      name: "Hubei",
+      color: "#F2F0DF",
+      description: "Covid-19 Started here in Wuhan. Do you want some bat soup too?",
+      hover_color: "#D65F14"
+    },
+    CHN1808: {
+      name: "Hunan",
+      color: "#B6B57D",
+      description: "Heard of \"P. F. Chang's China Bistro?\" Their first success dish is a Hunan cuisine.",
+      hover_color: "#D65F14"
+    },
+    CHN1809: {
+      name: "Sichuan",
+      color: "#D1B96D",
+      description: "This place is well-known for its spice.",
+      hover_color: "#D65F14"
+    },
+    CHN1810: {
+      name: "Yunnan",
+      color: "#BFB7B4",
+      description: "Once the mighty Kingdom of Dali, Yunnan has many mysterious tribes living in the area and each has its own traditions and cuisine.",
+      hover_color: "#D65F14"
+    },
+    CHN1811: {
+      name: "Hebei",
+      color: "#718682",
+      description: "Have you walked the Great Wall of China?",
+      hover_color: "#D65F14"
+    },
+    CHN1812: {
+      name: "Henan",
+      color: "#DDB46C",
+      description: "Located at the Central Plains of China; Many Leaders once set their capital here.",
+      hover_color: "#D65F14"
+    },
+    CHN1813: {
+      name: "Liaoning",
+      color: "#F19460",
+      description: "The people in Liaoning eat a lot because they need to build more Aircraft Carrier.",
+      hover_color: "#D65F14"
+    },
+    CHN1814: {
+      name: "Shandong",
+      color: "#9F8A76",
+      description: "The people here loves to eat food made from what flour such as noodles and dumplings",
+      hover_color: "#D65F14"
+    },
+    CHN1816: {
+      name: "Tianjin",
+      color: "#320A24",
+      description: "Tianjin people loves to sing and perform Opera.",
+      hover_color: "#D65F14"
+    },
+    CHN1817: {
+      name: "Jiangxi",
+      color: "#31CC6F",
+      description: "Are you a tea person? if so, this place is for you.",
+      hover_color: "#D65F14"
+    },
+    CHN1818: {
+      name: "Jiangsu",
+      color: "#309FD8",
+      description: "Come here if you like Chinese Style Architecture",
+      hover_color: "#D65F14"
+    },
+    CHN1819: {
+      name: "Shanghai",
+      color: "#196C60",
+      description: "The largest and most advanced City in China where the richest peole live in.",
+      hover_color: "#D65F14"
+    },
+    CHN1820: {
+      name: "Zhejiang",
+      color: "#A6CA83",
+      description: "Being close to the sea, they are known for cooking the best seafood dishes.",
+      hover_color: "#D65F14"
+    },
+    CHN1828: {
+      name: "Jilin",
+      color: "#333D51",
+      description: "Border to North Korea, this place is heavily influenced with Korean culture",
+      hover_color: "#D65F14"
+    },
+    CHN1838: {
+      name: "Inner Mongol",
+      color: "#9BB49E",
+      description: "Homeland of Mongols, where tasty Horse-Milk-Wine flows",
+      hover_color: "#D65F14"
+    },
+    CHN1839: {
+      name: "Heilongjiang",
+      color: "#A87A57",
+      description: "Homeland of Manchurian where the people used to hunt for living.",
+      hover_color: "#D65F14"
+    },
+    PFA00: {
+      name: "Paracel Islands",
+      color: "#29794F",
+      description: "Disputed territories, let's not talk about it",
+      hover_color: "#D65F14"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "39.928819",
+      lng: "116.388869",
+      name: "Beijing",
+      description: "Capital of China; Famous for the imperial food served for centuries."
+    }
+  },
+  labels: {},
+  regions: {}
+};
