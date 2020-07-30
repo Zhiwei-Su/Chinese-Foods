4aaed3be6bf708eeb147bae9059ae4a46455fe87
Titus Sudarno <43189799+titus-panjaya-sud@users.noreply.github.com>
2020-07-30 00:26:43 -0700

    Update Reference page

--- js/dishes.js
+++ js/dishes.js
@@ -13,4 +13,19 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+var coll = document.getElementsByClassName("collapsible2");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 // the following code is derived from https://www.w3schools.com/howto/howto_js_collapsible.asp
